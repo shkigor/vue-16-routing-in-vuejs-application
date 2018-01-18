a9fe36c294b2d08af8f54d94361b23eac8bf8905
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-18 23:24:53 +0200

    224 Setting Up and Loading Routes

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import { routes } from './routes'
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes
+  // routes: routes -- this ES6 do for us
+});
 
 new Vue({
   el: '#app',
+  router, // router: router -- as the same upper
   render: h => h(App)
 })
