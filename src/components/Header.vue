8b6aa5ebc078af692b80714a06454879cb417a6c
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-19 13:37:59 +0200

    226 Navigating with Router Links

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="nav nav-pills">
+    <li role="presentation"><router-link to="/">Home</router-link></li>
+    <li role="presentation"><router-link to="/user">User</router-link></li>
+  </ul>
+</template>
+
+<script>
+    export default {
+        name: "headerC" // VM1160:34 [Vue warn]: Do not use built-in or reserved HTML elements as component id: header
+    }
+</script>
+
+<style scoped>
+
+</style>
