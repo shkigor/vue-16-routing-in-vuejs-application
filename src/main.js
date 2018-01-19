6d22ee46d99e56a436563c9310f99e5b978d5b98
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-19 09:40:07 +0200

    225 Understanding Routing Modes Hash vs History
    https://router.vuejs.org/ru/essentials/history-mode.html

--- src/main.js
+++ src/main.js
@@ -6,8 +6,10 @@ import { routes } from './routes'
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes
   // routes: routes -- this ES6 do for us
+  routes,
+  // no /#/ hash tag style in the url (history mode). The default mode is 'hash'
+  mode: 'history'
 });
 
 new Vue({
