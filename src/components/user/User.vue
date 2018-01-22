3cd4a8a3d728fed4c2a50439cbe97632e6c8caa8
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-22 14:13:07 +0200

    revert to 228 lesson to clear our changes

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -2,19 +2,12 @@
     <div class="component">
         <h1>The User Page</h1>
         <hr>
-        <p>Loaded ID: {{ id }}</p>
         <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
 
 <script>
 export default {
-    props: {
-        id: {
-            type: String,
-            default: '123'
-        }
-    },
     methods: {
         navigateToHome() {
             this.$router.push('/');
