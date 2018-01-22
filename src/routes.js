b12903ce7f05cf7679122db5c439c1b8c7f0dddb
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-22 14:22:33 +0200

    232 Setting Up Child Routes Nested Routes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 import User from './components/user/User.vue';
+import UserDetail from './components/user/UserDetail.vue';
+import UserEdit from './components/user/UserEdit.vue';
+import UserStart from './components/user/UserStart.vue';
 import Home from './components/Home.vue';
 
 export const routes = [
     { path: '', component: Home },
-    { path: '/user', component: User }
+    { path: '/user', component: User, children: [
+        { path: '', component: UserStart },
+        { path: ':id/edit', component: UserEdit },
+        { path: ':id', component: UserDetail }
+      ] }
 ];
