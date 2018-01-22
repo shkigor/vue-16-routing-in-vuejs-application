46aa6e970d9550508132bc49f8250b6c175722ad
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-22 12:28:36 +0200

    228 Navigating from Code Imperative Navigation

--- src/components/user/User.vue
+++ src/components/user/User.vue
@@ -1,5 +1,18 @@
 <template>
     <div class="component">
         <h1>The User Page</h1>
+        <hr>
+        <button @click="navigateToHome" class="btn btn-primary">Go to Home</button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        navigateToHome() {
+            this.$router.push('/');
+            // this.$router.push( {path: '/'} );
+        }
+    }
+}
+</script>
