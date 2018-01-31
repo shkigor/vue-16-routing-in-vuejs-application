e87990c821d56419b865b4d79fdcb90107c39c57
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-31 13:25:46 +0200

    242 Controlling the Scroll Behavior

--- src/main.js
+++ src/main.js
@@ -9,7 +9,16 @@ const router = new VueRouter({
   // routes: routes -- this ES6 do for us
   routes,
   // no /#/ hash tag style in the url (history mode). The default mode is 'hash'
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return {selector: to.hash};
+    }
+    return {x: 0, y: 0};
+  }
 });
 
 new Vue({
