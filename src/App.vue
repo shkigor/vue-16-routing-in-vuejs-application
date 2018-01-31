9277181414d24a5b324cd06dfed3911ab4327f15
Igor Shklyar <igor.shklyar@gmail.com>
2018-01-31 10:49:14 +0200

    240 Animating Route Transitions

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
         <h1>Routing</h1>
         <hr>
         <router-view name="header-top"></router-view>
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+            <router-view></router-view>
+        </transition>
         <router-view name="header-bottom"></router-view>
       </div>
     </div>
@@ -26,5 +28,38 @@
   div.component {
     border: 1px solid black;
     padding: 30px;
+  }
+
+  .slide-leave-active {
+    transition: opacity 1s ease;
+    opacity: 0;
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  .slide-leave {
+    opacity: 1;
+    transform: translateX(0);
+  }
+
+  .slide-enter-active {
+    animation: slide-in 1s ease-out forwards;
+  }
+
+  @keyframes slide-out {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-30px);
+    }
+  }
+
+  @keyframes slide-in {
+    0% {
+      transform: translateY(-30px);
+    }
+    100% {
+      transform: translateY(0);
+    }
   }
 </style>
